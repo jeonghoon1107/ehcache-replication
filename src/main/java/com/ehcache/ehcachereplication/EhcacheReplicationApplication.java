b353d0a1af0035af0a4d57b9893485ca0c806fbe
jeonghoon <jeonghoon1107@gmail.com>
2018-07-26 09:58:39 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/ehcache/ehcachereplication/EhcacheReplicationApplication.java
@@ -0,0 +1,12 @@
+package com.ehcache.ehcachereplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcacheReplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EhcacheReplicationApplication.class, args);
+    }
+}
